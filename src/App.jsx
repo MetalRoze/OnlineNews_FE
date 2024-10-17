00e602fcbeeb0f82250e3f3e639d88dd8a081ea1
이은진 <intern05@gmail.com>
2024-10-17 14:54:30 +0900

    init: 라우팅 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'; //부트스트랩
+// src/App.jsx
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // 부트스트랩
+ 
 
-function App() {
-  const [count, setCount] = useState(0)
+import TestPage from './pages/TestPage';  
+
+function App() { 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <>   
+        {/* Routes 설정 */}
+        <Routes>
+          <Route path="/" element={<TestPage/>} /> 
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/testPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TestPage() {
+    return (
+        <div>
+            <h1>테스트 페이지</h1> 
+            <p>추후 홈 생기면 해당 페이지 삭제 후 App.jsx 경로 수정</p>
+        </div>
+    );
+}
