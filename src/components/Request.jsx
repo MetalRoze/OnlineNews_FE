369857abadf96503fd76f96023bae129fb2615ff
wlstj <22020306jinseoyeon@gmail.com>
2024-10-18 23:16:25 +0900

    style: request 컴포넌트 css 완성

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -4,21 +4,17 @@ import styled from 'styled-components';
 export default function Request() {
 
     return (
-        <StyledRequestWrapper className='flex column pd10 aiFlexend'>
-            {/* <div className='flex mb1' style={{border: '1px solid black'}}>
-                
-                <p className='m0' style={{border: '1px solid black'}}>10:30</p>
-            </div> */}
-            <h3 className='mb1'  style={{border: '1px solid black'}}>인텔 "최신 AI 칩 가우디3 탑재 서버, 국내 상륙 임박"</h3>
-            <div className='flex desktop-3buttons br10' style={{width: '70%'}}>
+        <div className='desktop-request pd10 aiFlexend'>
+            <div className='flex'>
+                <p>홍길동 기자</p>
+                <p className='taRight'>23:59</p>
+            </div>
+            <h4 className='mb1 ellipsis' style={{height: '5rem'}}>인텔 "최신 AI 칩 가우디3 탑재 서버, 국내 상륙 임박"</h4>
+            <div className='flex desktop-request-3buttons br10' style={{width: '70%'}}>
                 <button>승인</button>
                 <button>보류</button>
                 <button>거절</button>
             </div>
-        </StyledRequestWrapper>
+        </div>
     );
 }
-const StyledRequestWrapper = styled.div`
-    width: 25rem;
-    background-color: ${(props) => props.theme.colors.gray10};
-`;
--- src/pages/testPageDT.jsx
+++ src/pages/testPageDT.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Sidebar from '../components/Sidebar'
 import MyCalendar from '../components/Calendar';
 import Request from '../components/Request';
+import styled from 'styled-components';
 
 export default function TestPageDT() {
     return (
         <div className="flex" style={{width:"100vw"}}>
             <Sidebar />
             <div className="desktop-container">
-                <h1>테스트페이지</h1>
+                {/* <h1>테스트페이지</h1>
                 <h2>헤딩 2</h2>
                 <h3>헤딩 3</h3>
                 <h4>헤딩 4</h4>
@@ -24,11 +25,25 @@ export default function TestPageDT() {
                     <a href="#">이것은 링크입니다. hover 확인</a>
                 </p>
 
-                <button>기본 버튼</button>
-                <MyCalendar/>
-                <h2>기사 요청 현황</h2>
-                <Request></Request>
+                <button>기본 버튼</button> */}
+                <MyCalendar />
+                <div style={{ height: '3rem' }}></div>
+                <h2 className='mb1'>기사 요청 현황</h2>
+                <StyledRequestWrapper>
+                    <Request />
+                    <Request />
+                    <Request />
+                    <Request />
+                    <Request />
+                    <Request />
+                </StyledRequestWrapper>
             </div>
         </div>
     );
 }
+const StyledRequestWrapper = styled.div`
+    display: grid;
+    grid-template-rows: repeat(2, 1fr); 
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+`;
