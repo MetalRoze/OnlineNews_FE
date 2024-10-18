c585143d5f86e9b0a88c5054e61e61204d9d9142
eunjin <ejejnin@gmail.com>
2024-10-18 23:38:14 +0900

    feat: 기사 상세 헤더

--- src/pages/articleDetail/ArticleDetail.jsx
+++ src/pages/articleDetail/ArticleDetail.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import ArticleHeader from './ArticleHeader';
 
-
-const HeaderWrapper = styled.div`
-    width: 600px;
-    padding: 10px; 
-    background-color: var(--color-blue);
-    color: var(--color-white); 
-`;
+ 
 
 const ArticleDetail = () => {
-    const navigate = useNavigate();
-
+    const navigate = useNavigate(); 
     return (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-            <HeaderWrapper>
-                <div className='flex' style={{alignItems:"left"}}>
-                    <div>
-                    <i onClick={() => navigate(-1)} className="bi bi-chevron-left" 
-                      style={{ fontSize: '24px', cursor: 'pointer', marginRight: '10px' }} ></i></div>
-                    <h2>왕왕일보</h2>
-                </div>
-                <div className='flex spaceBetween'>
+        <div> 
+            <ArticleHeader/> 
+            <div className='mobile-container'>
 
-                </div>
-            </HeaderWrapper>
+
+            </div>
         </div>
 
     );
