e9af82be9415a68be7196fa51a1d4f4eabbdf468
wlstj <22020306jinseoyeon@gmail.com>
2024-10-18 23:40:06 +0900

    fix: request 상단 (기자이름 + 날짜) 수정

--- src/components/Request.jsx
+++ src/components/Request.jsx
@@ -5,9 +5,9 @@ export default function Request() {
 
     return (
         <div className='desktop-request pd10 aiFlexend'>
-            <div className='flex'>
+            <div className='flex spaceBetween' style={{width:'100%'}}>
                 <p>홍길동 기자</p>
-                <p className='taRight'>23:59</p>
+                <p>23:59</p>
             </div>
             <h4 className='mb1 ellipsis' style={{height: '5rem'}}>인텔 "최신 AI 칩 가우디3 탑재 서버, 국내 상륙 임박"</h4>
             <div className='flex desktop-request-3buttons br10' style={{width: '70%'}}>
