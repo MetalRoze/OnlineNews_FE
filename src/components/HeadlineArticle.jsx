c5d535537e30aae04b1bd1fb44b5ce3167800766
eunnnju <sbsdy3@gmail.com>
2024-10-20 21:16:54 +0900

    헤드라인 기사 컴포넌트 생성

---
+++ src/components/HeadlineArticle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function HeadlineArticle(){
+    return(
+        <div className='flex column mobile-header'>
+            <h2>수술 부위 감염되자 소송, 1심 패소 2심 승소…대법 판단은?</h2>
+            <h4>2심 의료상 과실 인정 "검출 병원균, 병원 내 감염 일으켜"</h4>
+            <h4>대법 "병원 감염 원인 다양…완전한 예방 불가" 파기 환송</h4>
+            <img style={{width:"35rem", height : "18rem"}}></img>
+
+            {/* 해당 이미지, 글 부분은 하드코딩말고 설정할 수 있도록 해야함.
+            그래서 재사용 컴포넌트로 만들었습니다...  */}
+        </div>
+    );
+}
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MenuList from "../../components/MenuList";
+import HeadlineArticle from "../../components/HeadlineArticle";
 
 export default function Main() {
     return(
-        <div className='mobile-header m0 pd0'>
+        <div className='flex column mobile-header m0 pd0'>
             <MenuList/>
+            <HeadlineArticle></HeadlineArticle>
         </div>
     );
 }
