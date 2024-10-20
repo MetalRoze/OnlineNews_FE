a0871861ed1ebdf448a8f41274448e1a4c5d633f
wlstj <22020306jinseoyeon@gmail.com>
2024-10-20 14:28:39 +0900

    style: pagination css 구성

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import Pagination from "react-js-pagination";
+
+export default function MyPagination() {
+    const [page, setPage] = useState(1);
+
+    const handlePageChange = (page) => {
+        setPage(page);
+    };
+    return (
+        <div>
+            <StyledPagination>
+                <Pagination
+                    activePage={1}
+                    itemsCountPerPage={5}
+                    totalItemsCount={300}
+                    pageRangeDisplayed={5}
+                    onChange={handlePageChange}
+                    prevPageText={"←"}
+                    nextPageText={"→"}>
+                </Pagination>
+            </StyledPagination>
+        </div>
+    );
+}
+const StyledPagination = styled.div`
+  .pagination {
+    display: flex;
+    justify-content: center;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    width: 2rem;
+    height: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10rem;
+    background-color: transparent;
+    border: none;
+
+    a {
+      text-decoration: none;
+      color: ${(props) => props.theme.colors.gray50};
+    }
+
+    &:hover,
+    &:active,
+    &.active {
+      background-color: ${(props) => props.theme.colors.black};
+      a {
+        color: ${(props) => props.theme.colors.white};
+      }
+    }
+
+    &:nth-child(2),
+    &:nth-last-child(2) {
+      background-color: transparent;
+
+      &:hover,
+      &:active,
+      &.active {
+        background-color: transparent;
+        a {
+          color: ${(props) => props.theme.colors.black};
+        }
+      }
+    }
+    &:first-child, 
+    &:last-child{
+        display: none;
+    }
+  }
+`;
