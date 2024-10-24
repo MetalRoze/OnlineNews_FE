1b821b7d4bcad4d89599ede22718d90aa07b0dc5
Eunjin <jin210907@naver.com>
2024-10-24 15:03:08 +0900

    feat: 광고 생성

--- src/components/GoogleAdsense.jsx
+++ src/components/GoogleAdsense.jsx
@@ -1,62 +1,56 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-const GoogleAdvertise = ({
-    className = "adsbygoogle",
-    client = "",
-    slot = "",
-    format = "",
-    responsive = "",
-    layoutKey = ""
-}) => {
+const GoogleAdsense = ({ adName }) => {
+    const adConfigs = {
+        AD560x300: {
+            width: 560,
+            height: 300,
+            slot: "2798382368",
+        },
+        AD580x150: {
+            width: 580,
+            height: 150,
+            slot: "1333229200",
+        },
+        AD560x150: {
+            width: 560,
+            height: 150,
+            slot: "6802040701",
+        },
+    };
+
+    const { width, height, slot } = adConfigs[adName] || {};
+
     useEffect(() => {
-        if (process.env.NODE_ENV === "production") {
-            const existingAd = document.querySelector(`ins.${className}[data-ad-client="${client}"][data-ad-slot="${slot}"]`);
-            if (!existingAd) {
-                try {
-                    (window.adsbygoogle = window.adsbygoogle || []).push({});
-                    console.log("Advertise is pushed");
-                } catch (e) {
-                    console.error("AdvertiseError", e);
-                }
+        const pushAd = () => {
+            try {
+                const adsbygoogle = window.adsbygoogle
+                adsbygoogle.push({})
+            } catch (e) {
+                console.error(e)
             }
         }
-    }, [className, client, slot]);
 
-    // 영역 미리보기
-    if (process.env.NODE_ENV !== "production") {
-        return (
-            <div
-                style={{
-                    background: "#e9e9e9",
-                    color: "black",
-                    fontSize: "18px",
-                    fontWeight: "bold",
-                    textAlign: "center",
-                    padding: "16px"
-                }}
-            >
-                광고 표시 영역
-            </div>
-        );
-    }
+        let interval = setInterval(() => {
+            if (window.adsbygoogle) {
+                pushAd()
+                clearInterval(interval)
+            }
+        }, 300)
 
-    // 광고 컴포넌트
+        return () => {
+            clearInterval(interval)
+        }
+    }, [adName])
+
     return (
         <ins
-            className={className}
-            style={{
-                overflowX: "auto",
-                overflowY: "hidden",
-                display: "block",
-                textAlign: "center"
-            }}
-            data-ad-client={client}
+            className="adsbygoogle"
+            style={{ display: "inline-block", width: `${width}px`, height: `${height}px`, backgroundColor: "var(--color-adsense)" }}
+            data-ad-client="ca-pub-1978575396259591"
             data-ad-slot={slot}
-            data-ad-format={format}
-            data-full-width-responsive={responsive}
-            data-ad-layout-key={layoutKey}
-        />
+        ></ins>
     );
 };
 
-export default GoogleAdvertise;
+export default GoogleAdsense;
