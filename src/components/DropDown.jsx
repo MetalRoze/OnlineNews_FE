bfd086cce0d1af91ec3c7451b42a77903105c3fe
wlstj <22020306jinseoyeon@gmail.com>
2024-10-25 18:12:50 +0900

    fix: dropdown 화면 빠져나가는 것 수정

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -4,7 +4,7 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 
 function MyDropdown() {
     return (
-        <CustomDropdown id="dropdown-basic-button" title="메뉴">
+        <CustomDropdown id="dropdown-basic-button" title="메뉴" drop={'start'}>
             <Dropdown.Item href="#/action-1">계정</Dropdown.Item>
             <Dropdown.Item href="#/action-2">알림</Dropdown.Item>
             <Dropdown.Item href="#/action-3">내 활동</Dropdown.Item>
@@ -13,21 +13,23 @@ function MyDropdown() {
 }
 
 const CustomDropdown = styled(DropdownButton)`
-
     #dropdown-basic-button{
         width: fit-content;
         padding: 0;
         background-color: transparent;
         color: ${(props) => props.theme.colors.gray60};
-        border: none;
-
+        border: none;   
+        &:hover, &:active{
+            color: ${(props) => props.theme.colors.black};
+        }
     }
-    .dropdown-toggle::after {
+    .dropdown-toggle::before {
         display: none;
     }
     .dropdown-menu{
         border: none;
         background-color: ${(props) => props.theme.colors.gray10};
+        box-shadow: 0.15rem 0 0.5rem rgba(0, 0, 0, 0.25);
     }
     .dropdown-item{
         color: ${(props) => props.theme.colors.gray60};
