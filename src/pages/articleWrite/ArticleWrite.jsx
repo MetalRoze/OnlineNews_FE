283c6da60def5a72768bb69c204b37ebe93befe6
eunjin <ejejnin@gmail.com>
2024-10-31 21:15:17 +0900

    feat: 에디터 이미지 크기 조절

--- src/pages/articleWrite/ArticleWrite.jsx
+++ src/pages/articleWrite/ArticleWrite.jsx
@@ -1,15 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import QuillEditor from './QuillEditor.jsx';
 
-const categories = ['랭킹', '정치', '경제', '사회', '연예', '생활/문화', '기계/IT', '오피니언'];
+const categories = ['정치', '경제', '사회', '연예', '생활/문화', '기계/IT', '오피니언'];
 
 const ArticleWrite = () => {
+    const [editorContent, setEditorContent] = useState('');
+
+    const handleEditorChange = (content) => {
+        setEditorContent(content); // QuillEditor에서 받은 내용을 상태로 저장
+    };
+
+    const handleSubmit = () => {
+        console.log('작성된 내용:', editorContent);
+    };
+
     return (
         <div className='mobile-container'>
-            <input type='text' className='mtb1' placeholder='제목을 입력해 주세요'></input>
-            <hr></hr>
-            <input className='mtb1' type='text' placeholder='소제목을 입력해 주세요'></input>
-            <hr></hr>
+            <input type='text' className='mtb1' placeholder='제목을 입력해 주세요' />
+            <hr />
+            <input className='mtb1' type='text' placeholder='소제목을 입력해 주세요' />
+            <hr />
             <div className='flex mtb1'>
                 <div className='mr1 mtbAuto'>카테고리</div>
                 <select>
@@ -20,12 +30,12 @@ const ArticleWrite = () => {
                     ))}
                 </select>
             </div>
-            <hr></hr>
-            <QuillEditor></QuillEditor>
+            <hr />
+            <QuillEditor onChange={handleEditorChange} />
             <div className='mlAuto'>
-                <button>승인 요청</button>
+                <button onClick={handleSubmit}>승인 요청</button>
             </div>
-        </div >
+        </div>
     );
 };
 
