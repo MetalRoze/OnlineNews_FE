89330580452c09a4b391bd1d12e90a87cc0cee7e
wlstj <22020306jinseoyeon@gmail.com>
2024-10-31 18:33:22 +0900

    feat: sidebar link이동

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,9 +10,11 @@ const Sidebar = () => {
             </div>
             <div className='list'>
                 <div className='ul'>
-                    <Link><i className="bi bi-search mr15 mb1"></i>기사 검색</Link>
-                    <Link><i className="bi bi-bell mr15 mb1"></i> 알림</Link>
-                    <Link><i className="bi bi-person mr15"></i> 내 정보</Link>
+                    <Link to="/articleManage"><i className="bi bi-search mr15 mb1"></i>기사 검색</Link>
+                    <Link to="/staffManage"><i className="bi bi-people mr15 mb1"></i>직원 관리</Link>
+                    <Link to="/requestManage"><i className="bi bi-question-circle mr15 mb1"></i>요청 관리</Link>
+                    <Link to="/desktopNoti"><i className="bi bi-bell mr15 mb1"></i>알림</Link>
+                    <Link to="/adminMypage"><i className="bi bi-person mr15"></i>내 정보</Link>
                 </div>
             </div>
             <div style={{ flex: "1" }}></div>
