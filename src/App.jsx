afe14eccbd4c48e954482260c1d140d12cf18526
wlstj <22020306jinseoyeon@gmail.com>
2024-11-01 23:12:26 +0900

    feat: cicd

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        제발요;;
+      </p>
     </>
   )
 }
