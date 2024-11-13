b253e3ec7c5ab5e4dd21449fcc481c8b04a42f87
eunjin <ejejnin@gmail.com>
2024-11-13 20:36:49 +0900

    feat: 기사 상세 조회 api 연결

--- src/pages/articleDetail/ArticleContent.jsx
+++ src/pages/articleDetail/ArticleContent.jsx
@@ -1,118 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import SubscriptionModal from './SubscriptionModal';
-import KakaoShare from '/src/utils/KakaoShare.jsx';
-import { useLocation } from 'react-router-dom';
+import KakaoShare from '/src/utils/KakaoShare.jsx'; 
 
-
-const ArticleContent = ({ article }) => {
-    const location = useLocation();
-    const isArticleDetail = location.pathname.includes('articleDetail');
-
-    const [articleLikeCount, setArticleLikeCount] = useState(0);
-    const [isArticleLiked, setIsArticleLiked] = useState(false);
-    const [isEmailSubscribed, setIsEmailSubscribed] = useState(true);
-
-    const [articleTitle, setArticleTitle] = useState('');
-    const [articleDate, setArticleDate] = useState('');
-    const [authorName, setAuthorName] = useState('');
-    const [authorEmail, setAuthorEmail] = useState('');
-    const [publisherUrl, setPublisherUrl] = useState('');
-    const [authorDescription, setAuthorDescription] = useState('');
-    const [articleSubtit, setArticleSubtit] = useState('');
-    const [subTitles, setSubTitles] = useState([]);
-    const [articleContent, setArticleContent] = useState([]);
-    useEffect(() => {
-        if (article) {
-            setArticleTitle(article.title || '');
-            setArticleDate(article.createdAt || '');
-            setAuthorName(article.authorName || '');
-            setAuthorEmail(article.authorEmail || '');
-            setPublisherUrl(article.publisherUrl || '');
-            setAuthorDescription(article.authorDescription || '');
-            setArticleSubtit(article.subtitle || '');
-            setArticleContent(article.content || '');
-        }
-    }, [article]);
-
-    useEffect(() => {
-        const splitSubtitles = articleSubtit.split(',./').map(sub => sub.trim()).filter(sub => sub !== "");
-        setSubTitles(splitSubtitles);
-    }, [articleSubtit]);
-
-    const handleArticleLikeToggle = () => {
-        setIsArticleLiked(prevState => {
-            const newState = !prevState;
-            setArticleLikeCount(prevCount => newState ? prevCount + 1 : prevCount - 1);
-            return newState;
-        });
-    };
-
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isSubscribed, setIsSubscribed] = useState(false);
-
-    const handleSubscriptionToggle = () => {
-        setIsModalOpen(true);
-    };
-
-    const handleSubscribe = () => {
-        setIsSubscribed(true);
-        setIsModalOpen(false);
-    };
-
-    const handleUnsubscribe = () => {
-        setIsSubscribed(false);
-        setIsModalOpen(false);
-    };
-    const handleEmailSubscribe = () => {
-        setIsEmailSubscribed(true);
-        setIsModalOpen(false);
-    };
-
-    const handleEmailUnsubscribe = () => {
-        setIsEmailSubscribed(false);
-        setIsModalOpen(false);
-    };
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://developers.kakao.com/sdk/js/kakao.js";
-        script.async = true;
-        document.body.appendChild(script);
-        return () => document.body.removeChild(script);
-    }, []);
-
+const ArticleContent = ({ 
+    article,
+    handleArticleLikeToggle,
+    handleSubscriptionToggle,
+    isArticleDetail,
+    isModalOpen,
+    isEmailSubscribed,
+    isArticleLiked,
+    isSubscribed,
+    setIsModalOpen,
+    handleSubscribe,
+    handleUnsubscribe,
+    handleEmailSubscribe,
+    handleEmailUnsubscribe
+}) =>{
 
     return (
         <div>
             <div>
                 <img src="https://placehold.co/130x50" alt="Bootstrap" />
-                <h1 className='mt1'>{articleTitle}</h1>
-                <small className='mt1 gray40'>{articleDate}</small>
+                <h1 className='mt1'>{article.title}</h1>
+                <small className='mt1 gray40'>{article.createdAt}</small>
                 <div className='mt1 taRight'>
-                    <p className='mr1 inline'>{authorName} 기자</p>
+                    <p className='mr1 inline'>{article.userName} 기자</p>
                     <img className='br50' src="https://placehold.co/50x50" alt="Author" />
                 </div>
             </div>
             <hr style={{ margin: '1rem 0' }} />
             <div>
-                {subTitles.map((subtitle, index) => (
-                    <h2 className='articleSubtitle' key={index}>{subtitle}</h2>
+                {article.subtitle.split(',./').map((sub, index) => (
+                    <h2 className='articleSubtitle' key={index}>{sub.trim()}</h2>  // 공백 제거
                 ))}
                 <br />
 
-                <div dangerouslySetInnerHTML={{ __html: articleContent }} />
+                <div dangerouslySetInnerHTML={{ __html: article.content }} />
             </div>
 
             {isArticleDetail && (
                 <>
-
                     <div className='mt1 flex'>
                         <img src="https://placehold.co/130x50" alt="Bootstrap" />
                         <div className='mlAuto'>
-                            <p className='m0'>{authorName} 기자 {authorEmail}</p>
-                            <small className='gray40'>{authorDescription}</small>
+                            <p className='m0'>{article.userName} 기자 {article.userEmail}</p>
+                            <small className='gray40'>{article.userBio}</small>
                         </div>
                     </div>
-                    <a className='gray40 mt1'>{publisherUrl} &gt;</a>
+                    <a className='gray40 mt1'>{article.publisherUrl} &gt;</a>
                     <button
                         className={`mt2 ${isSubscribed ? 'unsubsButton' : 'subsButton'}`}
                         onClick={() => {
@@ -137,9 +73,9 @@ const ArticleContent = ({ article }) => {
                                 className={`bi block taCenter ${isArticleLiked ? 'bi-heart-fill blue' : 'bi-heart'}`}
                                 onClick={handleArticleLikeToggle}
                             ></i>
-                            <small className='taCenter block'>{articleLikeCount}</small>
+                            <small className='taCenter block'>{article.likes}</small>
                         </div>
-                        <KakaoShare title={articleTitle} content={articleSubtit} link="articleDetail" THU="http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png" />
+                        <KakaoShare title={article.title} content={article.subtitle} link="articleDetail" THU="http://k.kakaocdn.net/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png" />
                     </div>
 
                     <SubscriptionModal
--- src/pages/articleDetail/ArticleDetail.jsx
+++ src/pages/articleDetail/ArticleDetail.jsx
@@ -6,16 +6,58 @@ import ArticleContent from './ArticleContent';
 import ArticleComment from './ArticleComment';
 import GoogleAdsense from '../../components/GoogleAdsense';
 import {getRequest}  from '../../apis/axios';
+import { useLocation } from 'react-router-dom';
 
 const ArticleDetail = () => {
     const { articleId } = useParams();
+
     const [article, setArticle] = useState(null);
     
+    const location = useLocation();
+    const isArticleDetail = location.pathname.includes('articleDetail');
+
+    const [isArticleLiked, setIsArticleLiked] = useState(false);
+    const [isEmailSubscribed, setIsEmailSubscribed] = useState(true);
+
+    const handleArticleLikeToggle = () => {
+        setIsArticleLiked(prevState => {
+            const newState = !prevState;
+            setLike(prevCount => newState ? prevCount + 1 : prevCount - 1);
+            return newState;
+        });
+    };
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isSubscribed, setIsSubscribed] = useState(false);
+
+    const handleSubscriptionToggle = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleSubscribe = () => {
+        setIsSubscribed(true);
+        setIsModalOpen(false);
+    };
+
+    const handleUnsubscribe = () => {
+        setIsSubscribed(false);
+        setIsModalOpen(false);
+    };
+    const handleEmailSubscribe = () => {
+        setIsEmailSubscribed(true);
+        setIsModalOpen(false);
+    };
+
+    const handleEmailUnsubscribe = () => {
+        setIsEmailSubscribed(false);
+        setIsModalOpen(false);
+    };
+
+    // 기사 가져오기
     const fetchArticle = async () => {
-        getRequest('/api/article/select')
+        getRequest('/api/article/select', { id: articleId })
             .then(response => {
                 setArticle(response.data[0]);
-                console.log(article)
             })
             .catch(error => {
                 console.error('Error fetching subscriptions:', error);
@@ -23,16 +65,41 @@ const ArticleDetail = () => {
     };
 
     useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "https://developers.kakao.com/sdk/js/kakao.js";
+        script.async = true;
+        document.body.appendChild(script);
+        return () => document.body.removeChild(script);
+    }, []);
+
+    useEffect(() => {
         if (articleId) { 
             fetchArticle();
         }
     }, [articleId]);
-
+    
+    if (!article) {
+        return <div>Loading...</div>; // 로딩 화면을 표시하거나, 로딩 상태에서 다른 처리를 할 수 있습니다.
+    }
     return (
         <div>
             <ArticleHeader />
             <div className='mobile-container pd20'>
-                <ArticleContent article={article} />
+                <ArticleContent
+                    article={article}
+                    handleArticleLikeToggle={handleArticleLikeToggle}
+                    handleSubscriptionToggle={handleSubscriptionToggle}
+                    isArticleDetail={isArticleDetail}
+                    isSubscribe={isSubscribed}
+                    isArticleLiked={isArticleLiked}
+                    isModalOpen={isModalOpen}
+                    isEmailSubscribed={isEmailSubscribed}
+                    setIsModalOpen={setIsModalOpen}
+                    handleSubscribe={handleSubscribe}
+                    handleUnsubscribe={handleUnsubscribe}
+                    handleEmailSubscribe={handleEmailSubscribe}
+                    handleEmailUnsubscribe={handleEmailUnsubscribe}
+                />
                 <ArticleComment />
                 <GoogleAdsense
                     client="ca-pub-1195209293008237"
