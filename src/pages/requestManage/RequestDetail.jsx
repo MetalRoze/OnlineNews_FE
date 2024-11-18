cba2df9d28cdf9bb7e6640339728bc417a37eeb3
wlstj <22020306jinseoyeon@gmail.com>
2024-11-18 22:58:44 +0900

    feat: request id로 요청 조회 api연결, article 조회 api연결

--- src/pages/requestManage/RequestDetail.jsx
+++ src/pages/requestManage/RequestDetail.jsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import BackgroundImage from '../../assets/staffDetailBackground.png';
-import ArticleDetail from '../articleDetail/ArticleDetail';
+import ArticleContent from '../articleDetail/ArticleContent';
+import ArticleHeader from '../articleDetail/ArticleHeader';
+import { getRequest } from '../../apis/axios';
 import { useParams } from 'react-router-dom';
 
 export default function RequestDetail() {
     const { id } = useParams();
+    const [request, setRequest] = useState();
+    const [article, setArticle] = useState();
+
+    //request api
+    const fetchRequestById = async (id) => {
+        try {
+            const response = await getRequest(`/api/request/${id}`);
+            setRequest(response.data);
+            console.log(response.data);
+
+            if (response.data.articleId) {
+                fetchArticleById(response.data.articleId);
+            }
+
+        } catch (error) {
+            console.error('요청실패', error);
+            setLoading(false);
+        }
+    };
+    //article api -> 추후 dto수정
+    const fetchArticleById = async (articleId) => {
+        try {
+            const response = await getRequest('/api/article/select', { id: articleId })
+            setArticle(response.data[0]);
+            console.log(response.data[0]);
+        } catch (error) {
+            console.error('기사 요청실패', error);
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchRequestById(id);
+    }, [id]);
+
     return (
         <div className="flex" style={{ width: "100vw" }}>
-            <div className="desktop-container aiCenter" style={{ padding: 0}}>
+            <div className="desktop-container aiCenter" style={{ padding: 0 }}>
                 <StyledBackground>
-                    <h1>홍길동 기자</h1>
-                    <p className='mb2'>example@example.com</p>
+                    {request && (
+                        <>
+                            <h1>{request.userName}</h1>
+                            <p className='mb2'>example@example.com</p>
+                        </>
+                    )}
                 </StyledBackground>
-                <div className='desktop-detail aiCenter boxShadow' style={{width: 'fit-content'}}>
-                    <div className='pd20'>
-                        {/* <ArticleDetail /> */}
+                <div className='desktop-detail aiCenter boxShadow' style={{ width: 'fit-content' }}>
+                    <div className='pd20 mobile-container'>
+                        {article && <ArticleContent article={article} />}
                     </div>
                     <div className='flex desktop-request-3buttons br10'>
                         <button>승인</button>
