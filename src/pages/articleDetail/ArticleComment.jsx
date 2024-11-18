8be72b09aecadfcd6b2e7c879b275e9633a333ea
eunjin <ejejnin@gmail.com>
2024-11-18 16:56:49 +0900

    feat: 댓글/답글 조회 api 연결

--- src/pages/articleDetail/ArticleComment.jsx
+++ src/pages/articleDetail/ArticleComment.jsx
@@ -1,30 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ArticleComment = () => {
+import { getRequest, postRequest, deleteRequest } from '../../apis/axios';
+const ArticleComment = ({
+    articleId
+}) => {
     const [newComment, setNewComment] = useState('');
     const [replyContent, setReplyContent] = useState('');
     const [comments, setComments] = useState([
         {
-            commentId: '01',
-            user: 'wangwang',
-            date: '2024.09.27 9:23',
+            id: '01',
+            userName: 'wangwang',
+            createdAt: '2024.09.27 9:23',
             content: 'Lorem, ipsum dolor sit amet consectetur.',
             likeCount: '511',
-            isActive: false,
             replies: [],
             isReplyVisible: false,
+            isActive: false,
         },
         {
-            commentId: '02',
-            user: 'malmal',
-            date: '2024.09.27 10:23',
+            id: '02',
+            userName: 'malmal',
+            createdAt: '2024.09.27 10:23',
             content: 'Lorem, ipsum dolor sit amet consectetur.',
             likeCount: '221',
-            isActive: false,
             replies: [],
             isReplyVisible: false,
+            isActive: false,
         },
     ]);
+
+    const fetchComment = async () => {
+        getRequest(`/api/comment/article/${articleId}`)
+            .then(response => {
+                // setComments(response.data)
+                const updatedComments = response.data.map(comment => ({
+                    ...comment, // 기존 댓글 데이터 유지
+                    isReplyVisible: false, // 새로운 필드 추가
+                    isActive: false       // 새로운 필드 추가
+                }));
+    
+                setComments(updatedComments)
+                console.log(updatedComments)
+            })
+            .catch(error => {
+                console.error('Error fetching subscriptions:', error);
+            });
+    };
 
     // 이름 변환
     const obfuscateUsername = (username) => {
@@ -61,7 +82,7 @@ const ArticleComment = () => {
     const toggleReplies = (commentId) => {
         setComments(prevComments =>
             prevComments.map(comment =>
-                comment.commentId === commentId ? { ...comment, isReplyVisible: !comment.isReplyVisible } : comment
+                comment.id === commentId ? { ...comment, isReplyVisible: !comment.isReplyVisible } : comment
             )
         );
     };
@@ -70,7 +91,7 @@ const ArticleComment = () => {
     const handleLikeToggle = (commentId) => {
         setComments(prevComments =>
             prevComments.map(comment =>
-                comment.commentId === commentId
+                comment.id === commentId
                     ? {
                         ...comment,
                         isActive: !comment.isActive,
@@ -100,7 +121,7 @@ const ArticleComment = () => {
             };
             setComments(prevComments =>
                 prevComments.map(comment =>
-                    comment.commentId === commentId
+                    comment.id === commentId
                         ? { ...comment, replies: [...comment.replies, newReplyData] }
                         : comment
                 )
@@ -111,11 +132,11 @@ const ArticleComment = () => {
     const handleReplyLikeToggle = (commentId, replyId) => {
         setComments(prevComments =>
             prevComments.map(comment =>
-                comment.commentId === commentId
+                comment.id === commentId
                     ? {
                         ...comment,
                         replies: comment.replies.map(reply =>
-                            reply.commentId === replyId
+                            reply.id === replyId
                                 ? {
                                     ...reply,
                                     isActive: !reply.isActive,
@@ -130,6 +151,12 @@ const ArticleComment = () => {
             )
         );
     };
+
+    useEffect(() => {
+        if (articleId) {
+            fetchComment();
+        }
+    }, [articleId]);
 
     return (
         <div className='mt2'>
@@ -150,23 +177,23 @@ const ArticleComment = () => {
             </div>
             <div className='pd10'>
                 {comments.map((comment) => (
-                    <div key={comment.commentId} className='mb1'>
+                    <div key={comment.id} className='mb1'>
                         <div className='flex'>
                             <img className='br50' src="https://placehold.co/40x40" alt="User Avatar" />
                             <div className='mtbAuto ml05'>
-                                <h6 className='m0'>{obfuscateUsername(comment.user)}</h6>
-                                <small className='gray40'>{comment.date}</small>
+                                <h6 className='m0'>{obfuscateUsername(comment.userName)}</h6>
+                                <small className='gray40'>{comment.createdAt}</small>
                             </div>
                         </div>
                         <p className='mt05'>{comment.content}</p>
                         <div className='flex mb1'>
-                            <small className={`cursor-pointer ${comment.isReplyVisible ? 'blue' : ''}`} onClick={() => toggleReplies(comment.commentId)}>
+                            <small className={`cursor-pointer ${comment.isReplyVisible ? 'blue' : ''}`} onClick={() => toggleReplies(comment.id)}>
                                 답글 {comment.replies.length} {comment.isReplyVisible ? <i className="bi bi-chevron-up"></i> : <i className="bi bi-chevron-down"></i>}
                             </small>
 
                             <i
                                 className={`bi block taCenter mlAuto ${comment.isActive ? 'bi-heart-fill blue' : 'bi-heart'}`}
-                                onClick={() => handleLikeToggle(comment.commentId)}
+                                onClick={() => handleLikeToggle(comment.id)}
                             ></i>
                             <small className='taCenter ml05'>{comment.likeCount}</small>
                         </div>
@@ -174,19 +201,19 @@ const ArticleComment = () => {
                         {comment.isReplyVisible && (
                             <div className='pdlr20'>
                                 {comment.replies.map((reply) => (
-                                    <div key={reply.commentId}>
+                                    <div key={reply.id}>
                                         <div className='flex'>
                                             <img className='br50' src="https://placehold.co/40x40" alt="User Avatar" />
                                             <div className='mtbAuto ml05'>
-                                                <h6 className='m0'>{obfuscateUsername(reply.user)}</h6>
-                                                <small className='gray40'>{reply.date}</small>
+                                                <h6 className='m0'>{obfuscateUsername(reply.userName)}</h6>
+                                                <small className='gray40'>{reply.createdAt}</small>
                                             </div>
                                         </div>
                                         <p className='mt05'>{reply.content}</p>
                                         <div className='flex'>
                                             <i
                                                 className={`bi block taCenter mlAuto ${reply.isActive ? 'bi-heart-fill blue' : 'bi-heart'}`}
-                                                onClick={() => handleReplyLikeToggle(comment.commentId, reply.commentId)}
+                                                onClick={() => handleReplyLikeToggle(comment.id, reply.id)}
                                             ></i>
                                             <small className='taCenter ml05'>{reply.likeCount}</small>
                                         </div>
@@ -202,7 +229,7 @@ const ArticleComment = () => {
                                 />
                                 <div className='flex'>
                                     <button
-                                        onClick={() => handleReplySubmit(comment.commentId)}
+                                        onClick={() => handleReplySubmit(comment.id)}
                                         className='blueButton mlAuto'
                                         style={{ marginTop: '5px' }}>
                                         답글 작성
