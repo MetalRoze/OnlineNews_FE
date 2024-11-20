60fcd73a025a4672c12be59f2408a5d2fbaeaed5
wlstj <22020306jinseoyeon@gmail.com>
2024-11-20 23:57:15 +0900

    feat: noti api 연결

--- src/pages/nofi/MobileNoti.jsx
+++ src/pages/nofi/MobileNoti.jsx
@@ -8,47 +8,59 @@ import { getRequest } from '../../apis/axios';
 
 export default function MobileNoti() {
     const [activeTab, setActiveTab] = useState('allNoties');
-    const [user, setUser] = useState(null);
+    const [userGrade,setUserGrade] = useState();
+    const [noties, setNoties] = useState([]);
     const [tabData, setTabData] = useState([]);
 
-    const fetchMyInfo = async () => {
-        try {
-            const response = await getRequest('/api/user/myPage');
-            console.log(response.data);
-        } catch (error) {
-            console.error('요청실패', error);
-        }
-    };
     const fetchMyGrade = async () => {
         try {
             const response = await getRequest('/api/user/checkUserType');
             console.log(response.data);
+            setUserGrade(response.data);
             if (response.data === 'REPORTER' || response.data=== 'INTERN_REPORTER' || response.data=== 'CITIZEN_REPORTER' ) {
                 setTabData([
-                    { eventKey: 'approveNoti', title: '승인알림', content: '승인알림' },
-                    { eventKey: 'commentNoti', title: '댓글등록', content: '댓글등록' },
-                    { eventKey: 'likeNoti', title: '좋아요알림', content: '좋아요알림' },
+                    { eventKey: 'allNoties', title: '전체', content: '전체알림' },
+                    { eventKey: 'REPORTER_APPROVAL_ACCEPTED', title: '승인', content: '승인알림' },
+                    { eventKey: 'REPORTER_COMMENT', title: '댓글', content: '댓글등록' },
+                    { eventKey: 'REPORTER_LIKE', title: '좋아요', content: '좋아요알림' },
                 ]);
             } else {
                 setTabData([
+                    { eventKey: 'allNoties', title: '전체', content: '전체알림' },
                     { eventKey: 'commentNoti', title: '댓글', content: '댓글알림' },
-                    { eventKey: 'replyNoti', title: '대댓글', content: '대댓글알림' },
+                    { eventKey: 'USER_REPLY', title: '대댓글', content: '대댓글알림' },
+                    { eventKey: 'USER_LIKE', title: '좋아요', content: '좋아요알림' },
                 ]);
             }
         } catch (error) {
             console.error('요청실패', error);
         }
     };
+
+    const fetchNoties = async (userGrade, status) => {
+        try {
+            const endpoint = status === 'allNoties'
+                ? '/api/notification/journalist'
+                : '/api/notification/journalist/type';
+            const params = status === 'allNoties' ? {} : { type: status };
+            const response = await getRequest(endpoint, params);
+            setNoties(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('요청실패', error);
+        }
+    };
+
     useEffect(() => {
         fetchMyGrade();
-        fetchMyInfo();
-    }, []);
+        fetchNoties(userGrade, activeTab);
+    }, [activeTab]);
     return (
         <div className="mobile-container">
             <DesktopTab tabData={tabData} setActiveTab={setActiveTab} />
             <TotalCount>전체 개</TotalCount>
-            {/* <StyledNotiListWrapper>
-                {noties[activeTab].map((noti) => (
+            <StyledNotiListWrapper>
+                {noties.map((noti) => (
                     <Notification
                         notiType={noti.notiType}
                         type={noti.type}
@@ -59,10 +71,10 @@ export default function MobileNoti() {
                         width={'100%'}
                     />
                 ))}
-            </StyledNotiListWrapper> */}
-            {/* <PaginationContainer>
-                <MyPagination itemsCountPerPage={10} totalItemsCount={noties[activeTab].length} pageRangeDisplayed={5} />
-            </PaginationContainer> */}
+            </StyledNotiListWrapper>
+            <PaginationContainer>
+                <MyPagination itemsCountPerPage={10} totalItemsCount={noties.length} pageRangeDisplayed={5} />
+            </PaginationContainer>
         </div>
     );
 }
