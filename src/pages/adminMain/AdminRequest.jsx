f892f1b65b6ad3962caf998e46d2d8aadcf37f17
wlstj <22020306jinseoyeon@gmail.com>
2024-11-20 13:32:57 +0900

    style: adminRequest 스타일 수정

--- src/pages/adminMain/AdminRequest.jsx
+++ src/pages/adminMain/AdminRequest.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import Label from '../../components/Label';
-import {convertUserGradeNumToKor} from '../../utils/convertUserGrade';
+import styled from 'styled-components';
+import { convertUserGradeNumToKor } from '../../utils/convertUserGrade';
 
 export default function AdminRequest({ request }) {
     console.log(request);
     return (
-        <div className='desktop-request pd10 aiCenter'>
-            <div className='flex spaceBetween mb1' style={{ width: '100%' }}>
-                <div className='flex' style={{gap:'0.5rem'}}>
-                    <p className='m0'>{request.userName} 기자</p>
-                    {convertUserGradeNumToKor(request.userGrade) === '시민기자' && <Label text="시민" />}
+        <div className='desktop-item pd10 aiCenter' >
+            <StyledArticleContentWrapper>
+                <div className='flex spaceBetween' style={{ width: '100%' }}>
+                    <div className='flex ' style={{ gap: '0.5rem' }} >
+                        <h5 className='m0'>{request.userName} 기자</h5>
+                        {convertUserGradeNumToKor(request.userGrade) === '시민기자' && <Label text="시민" />}
+                    </div>
+                    <p className='m0'>{request.createdAt.split("T")[0]}</p>
                 </div>
-                <p className='m0'>{request.createdAt.split("T")[0]}</p>
-            </div>
-            <h4 className='ellipsis'>{request.requestTitle===null? "아무말이나 하고 있습니다. 지금 db에는 아무 requestTitle이 없어서 너무 못생겼습니다.": request.requestTitle}</h4>
+                <p className='ellipsis m0 mt1'>{request.requestTitle === null ? "아무말이나 하고 있습니다. 지금 db에는 아무 requestTitle이 없어서 너무 못생겼습니다.dddddddddddddddddd" : request.requestTitle}</p>
+            </StyledArticleContentWrapper>
+
         </div>
     );
 }
+const StyledArticleContentWrapper = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-rows: 1fr 1.5fr;
+    justify-items: flex-start;
+`;
