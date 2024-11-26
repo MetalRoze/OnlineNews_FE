9d301ef2881d82b643a6721fcf99b472f7c857d2
Eunjin <jin210907@naver.com>
2024-11-26 15:25:29 +0900

    Feat: 기사 본문에 광고 추가

--- src/pages/articleDetail/ArticleDetail.jsx
+++ src/pages/articleDetail/ArticleDetail.jsx
@@ -17,7 +17,6 @@ const ArticleDetail = () => {
     const [isArticleLiked, setIsArticleLiked] = useState(false);
     const [likeId, setLikeId] = useState(null);
     const [categoryIdx, setCategoryIdx] = useState();
-    const [adError, setAdError] = useState(false);  // 광고 로드 오류 상태
 
     // 기사 가져오기
     const fetchArticle = async () => {
@@ -103,38 +102,6 @@ const ArticleDetail = () => {
         }
     }, [articleId]);
 
-    // 광고 스크립트 로드
-
-    useEffect(() => {
-        // 광고 스크립트 로드
-        const script = document.createElement("script");
-        const ins = document.createElement("ins");
-
-        ins.className = 'kakao_ad_area';
-        ins.setAttribute('style', 'display: none;');
-        script.async = true;
-        script.type = 'text/javascript';
-        script.src = '//t1.daumcdn.net/kas/static/ba.min.js';
-        ins.setAttribute('data-ad-unit', 'DAN-zuzxRmoWnjvO6oLm');
-        ins.setAttribute('data-ad-width', '300');
-        ins.setAttribute('data-ad-height', '250');
-
-        script.onload = () => {
-            console.log("광고 스크립트 로드 성공");
-            setAdError(false);
-        };
-
-        script.onerror = () => {
-            console.error("광고 스크립트 로드 실패");
-            setAdError(true);
-        };
-
-        let parent = document.getElementById('adFit');
-        parent?.appendChild(ins);
-        parent?.appendChild(script);
-
-    }, []);
-
     if (!article) {
         return <div>Loading...</div>;
     }
@@ -152,11 +119,6 @@ const ArticleDetail = () => {
                         isArticleLiked={isArticleLiked}
                     />
                 )}
-
-                <div>광고수정7</div>
-                <div id="adFit">
-                    {adError && <p>광고 로드 실패</p>}
-                </div>
 
                 <ArticleComment articleId={articleId} />
             </div>
