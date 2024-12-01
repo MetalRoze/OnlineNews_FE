bd6ef0e6b069a22d11d53133bdc41f96566275cf
wlstj <22020306jinseoyeon@gmail.com>
2024-12-01 17:47:08 +0900

    chore: tab 위치 수정

--- src/pages/articleManage/ArticleManage.jsx
+++ src/pages/articleManage/ArticleManage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import DesktopTab from '../../components/DesktopTab';
 import MyPagination from '../../components/Pagination';
-import {convertToKor} from '../../utils/convertCategories';
+import { convertToKor } from '../../utils/convertCategories';
 import { DesktopList } from '../../components/DesktopList';
 import { getRequest } from '../../apis/axios';
 
@@ -15,18 +15,18 @@ export default function ArticleManage() {
         { eventKey: 'createdAt', title: '등록순', content: '등록 순서 정렬' },
         { eventKey: 'views', title: '조회순', content: '조회순 높은 순으로 정렬' },
     ];
-    
+
     const fetchArticles = async (status) => {
         try {
-            const response = await getRequest('api/article/select', {sortBy: status, sortDirection:"desc"});
+            const response = await getRequest('api/article/select', { sortBy: status, sortDirection: "desc" });
             setArticles(response.data);
         } catch (error) {
             console.error('요청실패', error);
         }
     };
-    const headers = ["입력일자", "이름", "구분", "제목", "수정일자","작업"];
+    const headers = ["입력일자", "이름", "구분", "제목", "수정일자", "작업"];
     const columns = "1fr 0.8fr 0.8fr 2fr 1fr 0.8fr";
-    
+
     const startIdx = (currentPage - 1) * 12;
     const endIdx = startIdx + 12;
     const currentArticles = articles.slice(startIdx, endIdx);
@@ -36,7 +36,7 @@ export default function ArticleManage() {
         이름: article.userName,
         구분: convertToKor(article.category),
         제목: article.title,
-        수정일자: article.modifiedAt !==null ? article.modifiedAt.split("T")[0] : null,
+        수정일자: article.modifiedAt !== null ? article.modifiedAt.split("T")[0] : null,
         id: article.id,
         작업: "헤드라인",
     }));
@@ -53,14 +53,13 @@ export default function ArticleManage() {
     return (
         <div className="flex" style={{ width: "100vw" }}>
             <div className="desktop-container">
-                <div className='flex aiCenter spaceBetween mb1'>
-                    <h2>기사</h2>
+                <div className='flex aiCenter spaceBetween mt1'>
+                    <TotalCount>전체 {articles.length}개</TotalCount>
                     <div><DesktopTab tabData={tabData} setActiveTab={setActiveTab} /></div>
                 </div>
-                <TotalCount>전체 {articles.length}개</TotalCount>
 
                 <DesktopList pathTo={'../articleDetail'} contents={contents} headers={headers} columns={columns} />
-            
+
                 {articles.length !== 0 && (
                     <MyPagination itemsCountPerPage={12} totalItemsCount={articles.length} pageRangeDisplayed={5} onPageChange={handlePageChange} />
                 )}
--- src/pages/requestManage/RequestManage.jsx
+++ src/pages/requestManage/RequestManage.jsx
@@ -55,7 +55,7 @@ export default function RequestManage() {
         구분: request.type,
         제목: request.requestTitle,
         처리구분: convertStatusToKor(request.status),
-        승인일자: request.confirmedAt !== null ? request.confirmedAt.split("T")[0]: null,
+        승인일자: request.confirmedAt !== null ? request.confirmedAt.split("T")[0] : null,
         id: request.id,
     }));
 
@@ -64,11 +64,10 @@ export default function RequestManage() {
     return (
         <div className="flex" style={{ width: "100vw" }}>
             <div className="desktop-container">
-                <div className="flex aiCenter spaceBetween mb1">
-                    <h2>승인요청내역</h2>
+                <div className="flex aiCenter spaceBetween mt1">
+                    <TotalCount>전체 {requests.length}개</TotalCount>
                     <div><DesktopTab tabData={tabData} activeTab={activeTab} setActiveTab={setActiveTab} /></div>
                 </div>
-                <TotalCount>전체 {requests.length}개</TotalCount>
                 <div>
                     <DesktopList pathTo={'requestDetail'} contents={contents} headers={headers} columns={columns} />
                     {requests.length === 0 && (
