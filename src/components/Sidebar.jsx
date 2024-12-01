2a690ca14dd396f80055ca350edd12622312b289
wlstj <22020306jinseoyeon@gmail.com>
2024-12-01 18:51:12 +0900

    feat: active 추가

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { convertToEng } from '../utils/convertCategories';
 
 const Sidebar = ({ onCategorySelect }) => {
     const categories = ['전체', '정치', '경제', '사회', '연예', '생활/문화', '기계/IT', '오피니언'];
+    const [activeCategory, setActiveCategory] = useState('전체'); 
 
+    const handleCategoryClick = (category) => {
+        setActiveCategory(category); 
+        onCategorySelect(category === '전체' ? null : convertToEng(category));
+    };
     return (
         <div className="desktop-sidebar">
             <ul className="ul">
                 {categories.map((category, index) => (
-                    <li key={index} onClick={() => category === '전체'? onCategorySelect(null): onCategorySelect(convertToEng(category))}>
-                        {category}
-                    </li>
+                    <li
+                    key={index}
+                    className={activeCategory === category ? 'active' : ''} 
+                    onClick={() => handleCategoryClick(category)}
+                >
+                    {category}
+                </li>
                 ))}
             </ul>
         </div>
